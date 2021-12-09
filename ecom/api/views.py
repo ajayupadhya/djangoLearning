7495a9a0e4bc8d960d6b36dabf564048adeed382
ajay upadhyay <ajay.bhumca18@gmail.com>
2021-12-09 16:54:21 +0530

    first commit

---
+++ ecom/api/views.py
@@ -0,0 +1,10 @@
+from django.http import JsonResponse
+
+# Create your views here.
+
+
+def home(request):
+    return JsonResponse({
+        "info": "django course",
+        "name": "ajay"
+    })
